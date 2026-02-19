e4d0afbb08d2ab628c1819a13f87cde096575db8
mdashikulislam <ashik.nwu@gmail.com>
2026-02-20 01:24:45 +0600

    update

--- resources/views/helpers/forms/fields/captcha.blade.php
+++ resources/views/helpers/forms/fields/captcha.blade.php
@@ -7,7 +7,6 @@
 	$allVars = get_defined_vars();
 	$bladeInternalVars = ['__data', '__path', '__env', 'app', 'errors'];
 	$passedParams = array_diff_key($allVars, array_flip($bladeInternalVars));
-	
 	// Verify the field label
 	$label = $passedParams['label'] ?? '';
 	$label = (is_string($label) && !empty($label)) ? $label : null;
@@ -20,6 +19,9 @@
 	@if ($captchaType == 'recaptcha')
 		@include('helpers.forms.fields.recaptcha', $passedParams)
 	@endif
+    @if ($captchaType == 'turnstile')
+        @include('helpers.forms.fields.turnstile', $passedParams)
+    @endif
 	@if (in_array($captchaType, ['default', 'math', 'flat', 'mini', 'inverse', 'custom']))
 		@include('helpers.forms.fields.simple-captcha', $passedParams)
 	@endif
