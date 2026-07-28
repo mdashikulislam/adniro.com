bc560a3f8ffa0afb9aa5d05406ec6b236385b082
Ashikul Islam <ashik.nwu@gmail.com>
2026-07-29 02:46:42 +0600

    improve it

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
     
-    'driver' => env('SESSION_DRIVER', ''),
+    'driver' => env('SESSION_DRIVER', 'file'),
     
     /*
     |--------------------------------------------------------------------------
@@ -73,7 +73,7 @@ return [
     |
     */
     
-    'connection' => env('SESSION_CONNECTION', 'mysql'),
+    'connection' => env('SESSION_CONNECTION', null),
     
     /*
     |--------------------------------------------------------------------------
